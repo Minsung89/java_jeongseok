12778575dcfcd6ccd9027f52b66a7f3cf78420ce
misung89 <misung0217@gmail.com>
2020-04-07 15:50:32 +0900

    챕터3

---
+++ java_jeongseok/src/chapter3/OperatorEx10.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+public class OperatorEx10 {
+
+	public static void main(String[] args) {
+
+		byte a = 10;
+		byte b = 30;
+		byte c = (byte) (a * b);
+		System.out.println(c);
+		
+	}
+}
---
+++ java_jeongseok/src/chapter3/OperatorEx9.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+public class OperatorEx9 {
+
+	public static void main(String[] args) {
+
+		long a = 1000000 * 1000000;					
+		long b = 1000000 * 1000000L; 	 //long형 리터널					
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
